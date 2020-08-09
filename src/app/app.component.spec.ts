4d6aa53a1bec965fba9c9ea93420bfccef15bd22
Iván González <ivangonzalezmahagamage@gmail.com>
2020-08-09 20:46:59 +0200

    Fix empty test app-component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,4 +13,10 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
 });

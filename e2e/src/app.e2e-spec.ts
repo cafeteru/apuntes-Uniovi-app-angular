99e558d24efb55c08b9590821a05a8258d887a34
Iván González <ivangonzalezmahagamage@gmail.com>
2020-08-09 20:42:34 +0200

    Update dependencies

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('apuntes-Uniovi-app-angular app is running!');
+    expect(page.getTitleText()).toEqual('apuntes-Uniovi-app-angular app is running on FIREBASE!');
   });
 
   afterEach(async () => {

15ba04d8f052730e2bb5a233f43a375bf5d6925a
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2021-02-24 12:41:36 +0100

    Finish test validators

--- src/app/core/validators/phone-validator.spec.ts
+++ src/app/core/validators/phone-validator.spec.ts
@@ -1,13 +1,15 @@
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
 import { PhoneValidator } from './phone-validator';
 
 describe('PhoneValidator', () => {
   let formGroup: FormGroup;
+  let phone: AbstractControl;
 
   beforeEach(() => {
     formGroup = new FormGroup({
       phone: new FormControl('', PhoneValidator.isValid()),
     });
+    phone = formGroup.controls.phone;
   });
 
   it('create an instance', () => {
@@ -15,31 +17,31 @@ describe('PhoneValidator', () => {
   });
 
   it('input a empty value', () => {
-    formGroup.controls.phone.setValue('');
+    phone.setValue('');
     expect(formGroup.status).toBe('VALID');
   });
 
   it('input a null value', () => {
-    formGroup.controls.phone.setValue(null);
+    phone.setValue(null);
     expect(formGroup.status).toBe('VALID');
   });
 
   it('input a undefined value', () => {
-    formGroup.controls.phone.setValue(undefined);
+    phone.setValue(undefined);
     expect(formGroup.status).toBe('VALID');
   });
 
   it('input a invalid value', () => {
-    formGroup.controls.phone.setValue('test');
+    phone.setValue('test');
     expect(formGroup.status).toBe('INVALID');
-    formGroup.controls.phone.setValue('185632147');
+    phone.setValue('185632147');
     expect(formGroup.status).toBe('INVALID');
-    formGroup.controls.phone.setValue('98632147');
+    phone.setValue('98632147');
     expect(formGroup.status).toBe('INVALID');
   });
 
   it('input a valid value', () => {
-    formGroup.controls.phone.setValue('985632147');
+    phone.setValue('985632147');
     expect(formGroup.status).toBe('VALID');
   });
 });

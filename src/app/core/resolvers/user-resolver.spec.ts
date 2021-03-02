a933c74912c0062629c55315233e1a936ce34c07
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2021-03-02 18:04:59 +0100

    Fix resolver tests

--- src/app/core/resolvers/user-resolver.spec.ts
+++ src/app/core/resolvers/user-resolver.spec.ts
@@ -2,28 +2,31 @@ import { TestBed } from '@angular/core/testing';
 
 import { UserResolver } from './user-resolver.service';
 import { LoggerTestingModule } from 'ngx-logger/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { UserService } from '../services/user.service';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { User } from '../models/user';
-import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { environment } from '../../../environments/environment';
+import { ActivatedRoute, convertToParamMap, Router } from '@angular/router';
+import { userRoutes } from '../../components/user/user-routing.module';
 import { Location } from '@angular/common';
 import { TestUtils } from '../utils/test-utils';
-import { of } from 'rxjs';
 
 describe('UserResolver', () => {
   let resolver: UserResolver;
-  let route: ActivatedRoute;
-  let location: Location;
   let service: UserService;
+  let httpMock: HttpTestingController;
+  let route: ActivatedRoute;
+  let router: Router;
+  let location: Location;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         LoggerTestingModule,
         HttpClientTestingModule,
-        RouterTestingModule,
+        RouterTestingModule.withRoutes(userRoutes),
         TestUtils.getLanguages()
       ],
       providers: [
@@ -38,9 +41,15 @@ describe('UserResolver', () => {
       ]
     });
     resolver = TestBed.inject(UserResolver);
-    route = TestBed.inject(ActivatedRoute);
-    location = TestBed.inject(Location);
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
+    route = TestBed.inject(ActivatedRoute);
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
@@ -48,18 +57,22 @@ describe('UserResolver', () => {
   });
 
   it('check with existed user', () => {
+    const id = 1;
     const user = new User();
-    spyOn(service, 'findById').and.callFake(() => of(user));
     resolver.resolve(route.snapshot, null).subscribe(
       (res) => {
         expect(res).toBeTruthy();
         expect(res).toBe(user);
       }
     );
+    const req = httpMock.expectOne(`${environment.urlApi}/users/${id}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(user);
   });
 
   it('check with no existed user',
     () => {
+      const id = 1;
       spyOn(service, 'findById').and.callThrough();
       resolver.resolve(route.snapshot, null).subscribe(
         () => {
@@ -68,5 +81,8 @@ describe('UserResolver', () => {
           expect(location.path()).toBe('/users');
         }
       );
+      const req = httpMock.expectOne(`${environment.urlApi}/users/${id}`);
+      expect(req.request.method).toBe('GET');
+      req.error(new ErrorEvent('error'));
     });
 });

01ffff29b5f7c81aa7b7de7921eb5c127b269a33
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2021-03-27 23:57:54 +0100

    Add chart.js

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -2,6 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from '../../core/base/base.component';
 import { NGXLogger } from 'ngx-logger';
 import { TranslateService } from '@ngx-translate/core';
+import { UserService } from '../../core/services/user.service';
+import { ChartType } from 'chart.js';
+import { Label, MultiDataSet } from 'ng2-charts';
+import { marker } from '@biesbjerg/ngx-translate-extract-marker';
+
+const ROLE_TYPE_ADMIN = marker('role-type.admin');
+const ROLE_TYPE_STUDENT = marker('role-type.student');
+const ROLE_TYPE_TEACHER = marker('role-type.teacher');
+const USER_ACTIVE = marker('user.active');
+const USER_INACTIVE = marker('user.inactive');
 
 @Component({
   selector: 'app-menu',
@@ -9,19 +19,55 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent extends BaseComponent implements OnInit {
+  public userActiveLabel: Label[] = [];
+  public userActiveData: MultiDataSet = [[0, 0]];
+  statisticsType: ChartType = 'pie';
+
+  public userRoleLabel: Label[] = [];
+  public userRoleData: MultiDataSet = [[0, 0, 0]];
 
   constructor(
     protected logger: NGXLogger,
     protected translateService: TranslateService,
+    private userService: UserService
   ) {
     super(logger, translateService);
     this.logger.debug(MenuComponent.name, 'constructor()', 'start');
+    const keys = [
+      ROLE_TYPE_ADMIN,
+      ROLE_TYPE_TEACHER,
+      ROLE_TYPE_STUDENT,
+      USER_ACTIVE,
+      USER_INACTIVE
+    ];
+    this.translateService.get(keys).subscribe(
+      res => {
+        this.userActiveLabel = [
+          res[USER_ACTIVE],
+          res[USER_INACTIVE],
+        ];
+        this.userRoleLabel = [
+          res[ROLE_TYPE_ADMIN],
+          res[ROLE_TYPE_STUDENT],
+          res[ROLE_TYPE_TEACHER]
+        ];
+      }
+    );
     this.logger.debug(MenuComponent.name, 'constructor()', 'end');
   }
 
   ngOnInit(): void {
     this.logger.debug(MenuComponent.name, 'ngOnInit()', 'start');
+    this.userService.getStatistics().subscribe(
+      userStatistics => {
+        this.userActiveData = [
+          [userStatistics.active, userStatistics.inactive],
+        ];
+        this.userRoleData = [
+          [userStatistics.numAdmin, userStatistics.numStudents, userStatistics.numTeachers],
+        ];
+      }
+    );
     this.logger.debug(MenuComponent.name, 'ngOnInit()', 'end');
   }
-
 }
